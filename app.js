82760d1e0337d2eb92d8d947b009ef6a677fc7b8
dilpreet kaur <dilpreet.kukreja@gmail.com>
2020-06-24 23:57:56 -0700

    addding delete and edit buttons

--- app.js
+++ app.js
@@ -2,11 +2,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-var exphbs  = require('express-handlebars');
-const {loadMembers, saveMembers} = require('./loading-saving.js');
+var exphbs = require('express-handlebars');
+const {
+    loadMembers,
+    saveMembers
+} = require('./loading-saving.js');
 //const methodOverride = require('method-override');
 
+// var cors = require('cors');
 
+// app.use(cors());
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
@@ -15,22 +20,37 @@ app.set('view engine', 'handlebars');
 
 //Body Parser Middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-
-
+app.use(express.urlencoded({
+    extended: false
+}));
 
 //Members API routes
 app.use('/api/members', require('./membersController'));
 
-
 const membersArray = loadMembers();
-app.use('/', (req, res)=>{
-    res.render('index', {layout: 'main', members:loadMembers()});
+app.use('/edit/:id', (req, res) => {
+    console.log(req.query);
+    const id = req.params.id;
+    const name = req.query.name;
+    const email = req.query.email;
+    res.render('edit', {
+        layout: 'main',
+        id: id,
+        name: name,
+        email: email
+    });
 });
+app.use('/', (req, res) => {
+    res.render('index', {
+        layout: 'main',
+        members: loadMembers()
+    });
+});
+
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log(`Server listening at port: ${port}`);
 });
 
--- membersController.js
+++ membersController.js
@@ -2,120 +2,131 @@ const express = require('express');
 const router = express.Router();
 //const members = require('./members.js');
 const uuid = require('uuid');
+const shortid = require('shortid');
 
 const {loadMembers, saveMembers} = require('./loading-saving.js');
 
 //Get all members
-router.get('/', (req, res)=>{
-    console.log('Get all members')
-    const members = loadMembers();
-    console.log(members);
-    res.header("Content-Type",'application/json');
-    res.send(JSON.stringify(members, null, 2));
-    //res.send(members);
+router.get('/', (req, res) => {
+  console.log('Get all members');
+  const members = loadMembers();
+  console.log(members);
+  res.header('Content-Type', 'application/json');
+  res.send(JSON.stringify(members, null, 2));
+  //res.send(members);
 });
 
 //Get single member
-router.get('/:id', (req, res)=>{
-    console.log('Get a single member')
-    const members = loadMembers();
-    const id = req.params.id;
-    console.log(id);
-    const memberFound = members.find( member => {
-        return member.id == id;
-    });
-    //console.log(memberFound);
+router.get('/:id', (req, res) => {
+  console.log('Get a single member');
+  const members = loadMembers();
+  const id = req.params.id;
+  console.log(id);
+  const memberFound = members.find((member) => {
+    return member.id == id;
+  });
+  //console.log(memberFound);
 
-    if(!memberFound){
-        res.status(400).json({ Message: `No member with id of ${id}`});
-        return;
-    }
-    res.header("Content-Type",'application/json');
-    res.send(JSON.stringify(memberFound, null, 2));
-    //res.redirect('/');
+  if (!memberFound) {
+    res.status(400).json({
+      Message: `No member with id of ${id}`,
+    });
+    return;
+  }
+  res.header('Content-Type', 'application/json');
+  res.send(JSON.stringify(memberFound, null, 2));
+  //res.redirect('/');
 });
 
 //Create new member
-router.post('/create', (req, res)=>{
-    console.log('Post a new member')
-    const members = loadMembers();
-    console.log(req.body);
-    if(req.body.name && req.body.email){
-        const memberAlreadyExist = members.find( member =>{
-            return member.email === req.body.email;
-        });
-        if(!memberAlreadyExist){
-            const newMember = {
-                id: uuid.v4(),
-                name: req.body.name,
-                email: req.body.email
-            };
-            members.push(newMember);
-            saveMembers(members);
-             //res.send("User created successfully!")
+router.post('/create', (req, res) => {
+  console.log('Post a new member');
+  const members = loadMembers();
+  console.log(req.body);
+  if (req.body.name && req.body.email) {
+    const memberAlreadyExist = members.find((member) => {
+      return member.email === req.body.email;
+    });
+    if (!memberAlreadyExist) {
+      const newMember = {
+        //id: uuid.v4(),
+        id: shortid.generate(),
+        name: req.body.name,
+        email: req.body.email,
+      };
+      members.push(newMember);
+      saveMembers(members);
+      //res.send("User created successfully!")
+    } else {
+      res.status(400).json({
+        message: `Member with Email: ${req.body.email} already exits!!`,
+      });
+      return;
+    }
+  } else {
+    res.status(400).json({
+      message: 'Name and Email are both required fields!!',
+    });
+  }
+  //res.send(members);
 
-        }else{
-            res.status(400).json({message: `Member with Email: ${req.body.email} already exits!!`});
-            return;
-        }
-       
-    }else{
-        res.status(400).json({message: 'Name and Email are both required fields!!'});
-    } 
-    //res.send(members);
-   
-    res.redirect('/');
+  res.redirect('/');
 });
 
 //Update member
-router.put('/update/:id', (req, res)=>{
-    const members = loadMembers();
-    const id = req.params.id;
-    console.log('Update a exixting member');
-    //console.log(id);
-    const memberFound = members.find( member => {
-        return member.id == id;
-    });
-    //console.log(memberFound);
+router.put('/update/:id', (req, res) => {
+  const members = loadMembers();
+  const id = req.params.id;
+  console.log('Update a exixting member');
+  //console.log(id);
+  const memberFound = members.find((member) => {
+    return member.id == id;
+  });
+  //console.log(memberFound);
 
-    if(!memberFound){
-        res.status(400).json({ Message: `No member with id of ${id}`});
-        return;
+  if (!memberFound) {
+    res.status(400).json({
+      Message: `No member with id of ${id}`,
+    });
+    return;
+  }
+
+  members.forEach((member) => {
+    if (member.id == id) {
+      member.name = req.body.name ? req.body.name : member.name;
+      member.email = req.body.email ? req.body.email : member.email;
     }
-
-   members.forEach( member =>{
-        if(member.id == id){
-            member.name = req.body.name? req.body.name: member.name;
-            member.email = req.body.email? req.body.email: member.email;
-        }
-    });
-    saveMembers(members);
-    res.send('user data updated!')
-    //res.send(members);
+  });
+  saveMembers(members);
+  res.send('user data updated!');
+  //res.send(members);
 });
 
 //Delete Member
-router.delete('/delete/:id', (req, res)=>{
-    const members = loadMembers();
-    const id = req.params.id;
-    console.log("Deleting:",id);
-    const memberFound = members.find( member => {
-        return member.id == id;
+router.delete('/delete/:id', (req, res) => {
+  //   res.header('Access-Control-Allow-Origin', '*');
+  const members = loadMembers();
+  const id = req.params.id;
+  console.log('Deleting:', id);
+  const memberFound = members.find((member) => {
+    return member.id == id;
+  });
+  //console.log(memberFound);
+
+  if (!memberFound) {
+    res.status(400).json({
+      Message: `No member with id of ${id}`,
     });
-    //console.log(memberFound);
+    return;
+  }
 
-    if(!memberFound){
-        res.status(400).json({ Message: `No member with id of ${id}`});
-        return;
-    }
+  const indexOfMemberToRemove = members.indexOf(memberFound);
+  members.splice(indexOfMemberToRemove, 1);
+  saveMembers(members);
 
-    const indexOfMemberToRemove = members.indexOf(memberFound);
-    members.splice(indexOfMemberToRemove, 1);
-    saveMembers(members);
-    res.send('User deleted!')
-    //res.send(members);
-    //res.redirect('/');
+  res.send('User deleted!');
+  //res.send(members);
+  //res.redirect('/');
 });
-
 
 module.exports = router;
